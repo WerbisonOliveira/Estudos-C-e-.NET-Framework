59cf92ddee185cc9c8fe368950a3461b22b80304
Werbison Oliveira <werbison090@gmail.com>
2026-01-19 16:59:31 -0300

    Estudos sobre API

---
+++ API/web-api/Controllers/UsuariosController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Configuration;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web.Http;
+using web_api.Models;
+
+namespace web_api.Controllers
+{
+    [RoutePrefix("api/usuarios")]
+    public class UsuariosController : ApiController
+    {
+        private Tools.Logger logger;
+        private Repositories.Usuario user;
+
+        public UsuariosController()
+        {
+            logger = new Tools.Logger(ConfigurationManager.AppSettings["PathLog"]);
+            user = new Repositories.Usuario(ConfigurationManager.ConnectionStrings["conexao"].ConnectionString);
+        }
+
+        //POST: api/usuarios/register
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("register")]
+        public async Task<IHttpActionResult> Register([FromBody]Usuario usuario)
+        {
+            if (usuario == null)
+                return BadRequest("Preencha todos os campos!");
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                await user.RegisterAsync(usuario);
+                return Content(HttpStatusCode.Created, true);
+            }
+            catch (Exception ex)
+            {
+                logger.log(ex);
+                return InternalServerError();
+            }
+        }
+
+        //POST: api/usuarios/login
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("login")]
+        public async Task<IHttpActionResult> Login([FromBody]Usuario usuario)
+        {
+            if (usuario == null)
+                return BadRequest();
+
+            try
+            {
+                var auth = await user.FindUserAsync(usuario.Email, usuario.Senha);
+
+                if (auth == null)
+                    return NotFound();
+
+                return Ok(auth);
+            }
+            catch (Exception ex)
+            {
+                logger.log(ex);
+                return InternalServerError();
+            }
+        }
+
+    }
+}
---
+++ API/web-api/Repositories/Usuario.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Data;
+using System.Text;
+
+namespace web_api.Repositories
+{
+    public class Usuario
+    {
+        private SqlConnection conn;
+        private SqlCommand cmd;
+
+        public Usuario(string ConnectionString) 
+        {
+            conn = new SqlConnection(ConnectionString);
+
+            cmd = new SqlCommand();
+            cmd.Connection = conn;
+        }
+
+        public async Task<Boolean> RegisterAsync(Models.Usuario usuario)
+        {
+            int line = 0;
+
+            using (conn)
+            {
+                await conn.OpenAsync();
+
+                using (cmd)
+                {
+                    cmd.CommandText = "insert into usuarios (nome, email, senha) values (@nome, @email, @senha)";
+
+                    cmd.Parameters.Add(new SqlParameter("@nome", SqlDbType.VarChar)).Value = usuario.Nome;
+
+                    cmd.Parameters.Add(new SqlParameter("@email", SqlDbType.VarChar)).Value = usuario.Email;
+
+                    cmd.Parameters.Add(new SqlParameter("@senha", SqlDbType.VarChar)).Value = Convert.ToBase64String(Encoding.UTF8.GetBytes(usuario.Senha));
+
+                    line = await cmd.ExecuteNonQueryAsync();
+
+                    return line > 0;
+                }
+            }
+        }
+
+        public async Task<Models.Login> FindUserAsync(string email, string senha)
+        {
+            Models.Login auth = null;
+
+            using (conn)
+            {
+                await conn.OpenAsync();
+
+                using (cmd)
+                {
+                    cmd.CommandText = "select nome, senha from usuarios where email = @email";
+
+                    cmd.Parameters.Add(new SqlParameter("@email", SqlDbType.VarChar)).Value = email;
+
+                    SqlDataReader user = await cmd.ExecuteReaderAsync();
+
+                    using (user)
+                    {
+                        if (await user.ReadAsync())
+                        {
+                            string Senha = user["senha"].ToString();
+
+                            if (Senha == Convert.ToBase64String(Encoding.UTF8.GetBytes(senha)))
+                            {
+                                auth = new Models.Login();
+                                auth.Nome = user["nome"].ToString();
+                                auth.Auth = true;
+                            }
+
+                        }
+                    }
+
+                }
+            }
+
+            return auth;
+        }
+
+    }
+}
